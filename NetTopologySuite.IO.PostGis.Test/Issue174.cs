72c2a0acd4a92fb1adf5f63a28d68b4f52c482a8
Ben Abelshausen <ben@osmsharp.com>
2018-02-27 09:53:06 +0000

    Migrated strong naming test.

---
+++ NetTopologySuite.IO.PostGis.Test/Issue174.cs
@@ -0,0 +1,23 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace NetTopologySuite.IO.PostGis.Test
+{ 
+    class Issue174
+    {
+        [Test, Category("Issue174")]
+        public void ensure_NetTopologySuite_IO_GeoJSON_assembly_is_strongly_named()
+        {
+            AssertStronglyNamedAssembly(typeof(PostGisReader));
+        }
+
+        private void AssertStronglyNamedAssembly(Type typeFromAssemblyToCheck)
+        {
+            Assert.IsNotNull(typeFromAssemblyToCheck, "Cannot determine assembly from null");
+            Assembly assembly = typeFromAssemblyToCheck.Assembly;
+            StringAssert.DoesNotContain("PublicKeyToken=null", assembly.FullName, "Strongly named assembly should have a PublicKeyToken in fully qualified name");
+        }
+    }
+}
